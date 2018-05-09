b1fe4ca375128b11851e8c267bcc595813843489
Anil Choudhary <choudharya335@gmail.com>
2018-05-09 14:11:58 +0530

    pdf addded

--- react-router-v4/Demo-02-Nested-routing/src/Category.jsx
+++ react-router-v4/Demo-02-Nested-routing/src/Category.jsx
@@ -5,12 +5,16 @@ import { Link, Route } from 'react-router-dom';
 
 const Category = ({ match }) => {
 return( <div>   <ul>
-        <li><Link to={`${match.url}/shoes`}>Shoes</Link></li>
-        <li><Link to={`${match.url}/boots`}>Boots</Link></li>
-        <li><Link to={`${match.url}/footwear`}>Footwear</Link></li>
+        <li><Link to={`${match.url}/shoes`}>Shoes1</Link></li>
+        <li><Link to={`${match.url}/boots`}>Boots1</Link></li>
+        <li><Link to={`${match.url}/footwear`}>Footwear1</Link></li>
    
       </ul>
-      <Route path={`${match.path}/:name`} render= {({match}) =>( <div> <h3> {match.params.name} </h3></div>)}/>
+      <Route path={`${match.path}/:name`} render= {({match}) => {
+      console.log(match);
+      return(<div> <h3> {match.params.name} </h3></div>);
+      
+}}/>
       </div>)
 }
       
